5fb697943bb43273e1cbb761a8f6520c58613936
sdmule <mulesaurabh45@gmail.com>
2026-07-11 12:34:48 +0530

    React is declarative

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
 
+// createRoot(document.getElementById('root')).render(
+//   <h1>
+//     Hello!!
+//   </h1>
+// )
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    Hello!!
-  </StrictMode>
-)
+const rootElement = document.getElementById('root');
+
+const h1Element = document.createElement("h1")
+
+h1Element.textContent = "Hello World!!";
+
+rootElement.appendChild(h1Element)
